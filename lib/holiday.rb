8edf9cc2e4264da4e1c8cdeeb2fbc2e92935c43a
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 16:51:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,6 +90,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |holiday, season|
     holiday.each do |holiday_name, supply|
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
