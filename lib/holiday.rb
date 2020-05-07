ba2baa59388ea2fe04b3d49fd0c5bb27526dba4a
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 14:10:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash.each do|seasons, holiday|
     if seasons == season
       holiday.each do |season_holidays, props|
-        season_holidays <<holiday_name[supply_array]
+        season_holidays << holiday_name[supply_array]
+        holiday_hash
       end
     end
   end
