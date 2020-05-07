79c3f17f3dc5f018411f843b316b1dce4d7c1e3e
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 16:37:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
     holiday_hash.each do |season, holiday|
       if season == :winter
-        holiday.values.flatten
+        season.values.flatten
       end
     end
 
