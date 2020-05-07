5feb0519e6f257078331cc7981ec79bb56d94243
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 16:41:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_winter_holiday_supplies(holiday_hash)
         array
      end
     end
-     holiday_hash[:winter].values.flatten
+  # holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
