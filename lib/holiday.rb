cfcc9ce954ec3ecda025b6c2c09c747cc8acf20d
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 17:36:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -102,7 +102,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday_name, supply|
-      holiday_name if supply.include?("BBQ")
+      if supply.include?("BBQ")
       end
     end
   end.flatten.compact
