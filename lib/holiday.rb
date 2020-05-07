1d0982a418aab88d24e8d7d165bf008f67e2a888
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 17:03:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,6 +89,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
+    "#{season.to_string.capitalize}"
     holiday.each do |holiday_name, supply|
     end
   end
