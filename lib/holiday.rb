8f2c72663d71fb635d36fd12607a585c7c18c685
Jackson Ramos <jackramos021495@gmail.com>
2020-05-07 16:21:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 #      holiday_hash
 #end
      holiday_hash[season][holiday_name] = supply_array
-
+     holiday_hash
 end
 
 
